3bdd90aa0b31b4db9635714a88755bc12d5be21a
tuanh78 <phamtuanh2000@gmail.com>
2021-03-23 21:55:40 +0700

    fix search product

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -32,30 +32,37 @@
                         <span>Có @php echo(count($products)) @endphp kết quả phù hợp</span>
                     </p>
                 </div>
-                <div class="list-product-by-brand row" >
-                    @foreach ($products as $product)
-                        <div class="new-product_item">
-                            <div class="item_image">
-                                <img src="{{ asset('images/products/product' . $product->id . '.jpg') }}" width="90%" alt="">
-                                <div class="item_image_mask">
-                                    <div class="animation3d">
-                                        <div>
-                                            <i class="fas fa-search-plus"></i>
-                                        </div>
-                                        <div class="js-add-to-cart">
-                                            <i class="fas fa-shopping-basket iconcart"></i>
+                @if(!empty($products))
+                    <div class="list-product-by-brand row">
+                        @foreach ($products as $product)
+                            <div class="new-product_item">
+                                <div class="item_image">
+                                    <img src="{{ asset('storage/' . $product->po_image) }}"
+                                         width="90%" alt="">
+                                    <div class="item_image_mask">
+                                        <div class="animation3d">
+                                            <div>
+                                                <i class="fas fa-search-plus"
+                                                   data-route="{{ route('get.ajax.product', $product->id) }}"></i>
+                                            </div>
+                                            <div class="js-add-to-cart">
+                                                <i class="fas fa-shopping-basket iconcart"></i>
+                                            </div>
                                         </div>
                                     </div>
+                                    <img src="{{ asset('images/icons/options.png') }}" alt="" class="item_image_option">
                                 </div>
-                                <img src="{{ asset('images/icons/options.png') }}" alt="" class="item_image_option">
+                                <div class="item_info">
+                                    <a href="{{ route('get.product.show', $product->id) }}"
+                                       class="item_info_name">{{ $product->po_name }}</a>
+                                    <p class="item_info_price">@php echo number_format($product->po_price, 0, '', '.') . 'đ'; @endphp
+                                        ₫</p>
+                                </div>
                             </div>
-                            <div class="item_info">
-                                <a href="{{ route('get.product.show', 1) }}" class="item_info_name">{{ $product->po_name }}</a>
-                                <p class="item_info_price">@php echo number_format($product->po_price, 0, '', '.') . 'đ'; @endphp₫</p>
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
+                        @endforeach
+                    </div>
+                @endif
+
             </div>
         </div>
     </div>
