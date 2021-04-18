6cf02a39a0923a549f3d8c787b2b22d22695b0f3
Quang Ha <quangha2611@gmail.com>
2021-04-18 22:37:27 +0700

    Before merge

--- app/Http/Controllers/VoyagerStatisticsController.php
+++ app/Http/Controllers/VoyagerStatisticsController.php
@@ -3,10 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use TCG\Voyager\Facades\Voyager;
 
 class VoyagerStatisticsController extends \TCG\Voyager\Http\Controllers\VoyagerBaseController
 {
-    public function index(Request $request) {
-        return 'jdsflksdjlk';
-    }
+    
 }
